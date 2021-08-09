096dc483a6dbe9f3fdf3c62e8cd1d30cd57b7703
Shivam Piyush <shivam.piyush@jungleegames.com>
2021-08-09 22:06:50 +0530

    issue fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Footer.css';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
-import { MDBIcon, MDBContainer } from 'mdbreact';
 
 function Footer() {
   return (
