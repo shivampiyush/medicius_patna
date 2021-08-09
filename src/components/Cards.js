6950b0b62b5dba44c6c1dd6cb5832797e86bbd97
Aditya Abhinav <aditya.avinav27dec@gmail.com>
2021-08-09 21:56:39 +0530

    added social media icons

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,20 +9,6 @@ function Cards() {
       <hr />
       <div className='cards__container'>
         <div className='cards__wrapper'>
-          {/* <ul className='cards__items'>
-            <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
-            />
-            <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
-            />
-          </ul> */}
           <ul className='cards__items'>
             <CardItem
               src='images/img-1.webp'
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Footer.css';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
+import { MDBIcon, MDBContainer } from 'mdbreact';
 
 function Footer() {
   return (
@@ -9,9 +10,9 @@ function Footer() {
       <section className='footer-subscription'>
         <h1 className='footh'>Contact Us</h1>
         <div className='divfoot'>
-          <h3>Better yet, see us in person!</h3>
           <p className='footer-subscription-heading'>
-            We love&#10084;&#65039; our customers, so feel free to reach us during normal business hours.
+            {/* We love&#10084;&#65039; our customers, so feel free to reach us during normal business hours. */}
+            Times may be tough, but Medicius will be there for you and your medical requirements. The hassle of waiting for multiple days is now over as we are focused to provide your medical essentials in just few hours.
           </p>
           <div>
             <a href="https://wa.me/917759995200/?" title="click to open whatsapp chat" target="_blank"><button class="btn btn-success">
@@ -19,7 +20,7 @@ function Footer() {
             </button></a>
           </div>
           <div className='table'>
-            <h3 className='hours'>Hours</h3>
+            <h3 className='hours'>Delivery Hours</h3>
             <table>
               <tr>
                 <td>Monday</td>
@@ -50,6 +51,15 @@ function Footer() {
                 <td>10:00 am – 08:00 pm</td>
               </tr>
             </table>
+          </div>
+          <div className='connect'>
+            <h3 className='connectText'>Connect with us: </h3>
+            <ul>
+              <li><a href='https://www.facebook.com/MediciusPatna' className='fa fa-facebook socially' target="_blank"></a></li>
+              <li><a href='https://twitter.com/Medicius_?s=08' className='fa fa-twitter socially' target="_blank"></a></li>
+              <li><a href='https://instagram.com/medici_us?utm_medium=copy_link' className='fa fa-instagram socially' target="_blank"></a></li>
+              {/* <li><a href='mailto:medicius.care@gmail.com' target="_blank"><span class="iconify" data-icon="logos:google-gmail" data-width='30' data-height='30'></span></a></li> */}
+            </ul>
           </div>
         </div>
       </section>
